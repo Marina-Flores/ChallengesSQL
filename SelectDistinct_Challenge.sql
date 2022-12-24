e726e5715935793434a6ec0bae7028aaf57b8c5b
Marina Flores <mlbtf@discente.ifpe.edu.br>
2022-12-24 15:40:42 -0300

    select with distinct challenge

---
+++ SelectDistinct_Challenge.sql
@@ -0,0 +1,13 @@
+-- Use a SELECT DISTINCT statement to get the distinct (i.e. unique) values that occur in the locality column from the bird.antarctic_populations table.
+
+SELECT DISTINCT 
+		locality 
+FROM 
+		bird.antarctic_populations (NOLOCK);
+
+--Use a SELECT DISTINCT statement to get the distinct combinations of values for both the locality and species_id columns from the bird.antarctic_populations table 
+
+SELECT DISTINCT 
+		locality, 
+		species_id 
+FROM bird.antarctic_populations (NOLOCK);
